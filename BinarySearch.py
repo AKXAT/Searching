4b3796644de87b5f172cb0fa6f74f589cfbcd8cc
unknown <akshat.aksharma@gmail.com>
2021-02-03 15:47:43 +0530

    Adding Recursion in Binary Search

--- BinarySearch.py
+++ BinarySearch.py
@@ -11,9 +11,25 @@ def binary_search(mylist,value):
         else:
             right_index = mid_index -1 
     return None 
+def binary_search_rec(mylist,value,left_index,right_index):
+    if right_index < left_index:
+        return None
+
+    mid_index = (left_index + right_index) // 2
+    mid_number = mylist[mid_index]
+
+    if mid_number == value:
+        return mid_index
+    if mid_number < value :
+        left_index = mid_index + 1
+    else:
+        right_index = mid_index -1 
+    return binary_search_rec(mylist,value,left_index,right_index)
+
 if __name__ == '__main__' :
     mylist = [12,23,34,45,55,56,78,89,90]
-    value = 550
+    value = 89
     myfunc = binary_search(mylist,value)
-    
+    recfunc = binary_search_rec(mylist,value,0,8)
     print(f"The value = {value} is present at the Index = {myfunc}")
+    print(f"The value = {value} is present at the Index = {recfunc}")
