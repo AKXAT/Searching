1fc7505ee8d577cac16286e34f501cce79493383
unknown <akshat.aksharma@gmail.com>
2021-02-04 15:48:52 +0530

    Fixed minimum iterations

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,11 +1,12 @@
 def bubble_sort(mylist):
     size = len(mylist)
     for k in range(size-1):
-        for i in range (size-1):
+        for i in range (size-1-k):
             if mylist[i]>mylist[i+1]:
                 temp = mylist[i]
                 mylist[i] = mylist[i+1]
                 mylist[i+1]=temp
+        
     return mylist
 
 
