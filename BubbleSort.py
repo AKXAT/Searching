e7b7767e6db8c5b890cc7b52ba02ecc146c98037
unknown <akshat.aksharma@gmail.com>
2021-02-04 15:45:55 +0530

    First Commit

---
+++ BubbleSort.py
@@ -0,0 +1,14 @@
+def bubble_sort(mylist):
+    size = len(mylist)
+    for k in range(size-1):
+        for i in range (size-1):
+            if mylist[i]>mylist[i+1]:
+                temp = mylist[i]
+                mylist[i] = mylist[i+1]
+                mylist[i+1]=temp
+    return mylist
+
+
+if __name__ =='__main__':
+    mylist = [3,5,1,2,3,89,0,4,34]
+    print(bubble_sort(mylist))
